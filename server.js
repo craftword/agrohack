e2af1dc5b9825189e20e35e30ab35dc155da65ec
Abimbola Olaitan <craftwordltd@gmail.com>
2019-05-03 03:01:05 +0100

    modify server js

--- server.js
+++ server.js
@@ -39,10 +39,10 @@ app.prepare()
     return handle(req, res)
   })
       
-  server.listen(3000, (err) => {
-    if (err) throw err
-    console.log('> Ready on http://localhost:3000')
-  })
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+      console.log(`Our app is running on port ${ PORT }`);
+  });
 })
 .catch((ex) => {
   console.error(ex.stack)
