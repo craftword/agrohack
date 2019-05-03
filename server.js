6ae6ba301104dcacdc632faa4fd8e6b668e42288
Abimbola Olaitan <craftwordltd@gmail.com>
2019-05-03 03:30:36 +0100

    modify server js

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.prepare()
   })
       
   const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
+  server.listen(PORT, () => {
       console.log(`Our app is running on port ${ PORT }`);
   });
 })
